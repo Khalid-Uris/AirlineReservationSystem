87895b5a0f4a2ba7de59b69b3bea286738ff456a
Khalid Uris <uriskhalid@gmail.com>
2020-08-08 07:11:43 +0500

    Add project files.

---
+++ AirlineReservationSystem/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace AirlineReservationSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.usertxt = new System.Windows.Forms.TextBox();
+            this.passtxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "User Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Password";
+            // 
+            // usertxt
+            // 
+            this.usertxt.Location = new System.Drawing.Point(135, 62);
+            this.usertxt.Name = "usertxt";
+            this.usertxt.Size = new System.Drawing.Size(192, 20);
+            this.usertxt.TabIndex = 2;
+            // 
+            // passtxt
+            // 
+            this.passtxt.Location = new System.Drawing.Point(135, 102);
+            this.passtxt.Name = "passtxt";
+            this.passtxt.PasswordChar = '*';
+            this.passtxt.Size = new System.Drawing.Size(193, 20);
+            this.passtxt.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(135, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Login_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(501, 269);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.passtxt);
+            this.Controls.Add(this.usertxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox usertxt;
+        private System.Windows.Forms.TextBox passtxt;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AirlineReservationSystem/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AirlineReservationSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            AirlineDbEntities db = new AirlineDbEntities();
+            if(usertxt.Text !=string.Empty && passtxt.Text !=string.Empty)
+            {
+                var user = db.Admins.Where(a => a.Username.Equals(usertxt.Text)).SingleOrDefault();
+                if(user!=null)
+                {
+                    if(user.Password.Equals(passtxt.Text))
+                    {
+                        MessageBox.Show("Login Admin");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid Password");
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Username");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please fill Username & Password");
+            }
+        }
+    }
+}
